9eed91798a8cb3b21eee383ac6d584d66f6b6cb4
ramgopal <72145167+ramgopal99@users.noreply.github.com>
2024-11-24 05:00:30 +0530

    Update secure.php

--- secure.php
+++ secure.php
@@ -16,11 +16,9 @@ if ($conn->connect_error) {
 
 // Get user input from a POST request (use POST for login forms, not GET)
 $user_input_username = $_POST['username'];
-$user_input_password = $_POST['password'];
 
 // Secure input sanitization
 $user_input_username = htmlspecialchars($user_input_username, ENT_QUOTES, 'UTF-8');
-$user_input_password = htmlspecialchars($user_input_password, ENT_QUOTES, 'UTF-8');
 
 $stmt->bind_param("ss", $user_input_username, $user_input_password); // 'ss' means both are strings
 
